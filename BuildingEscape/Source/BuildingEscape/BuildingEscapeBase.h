2c32eb97007a3829184cca61909a0c39284f475e
Andrew Morrison <andy.wmorrison@gmail.com>
2017-07-07 01:08:07 -0700

    add grabber and game base

---
+++ BuildingEscape/Source/BuildingEscape/BuildingEscapeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_API ABuildingEscapeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
