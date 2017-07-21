91b999bad1f7ca60f65bd00292713c57208f52e2
Andrew Morrison <andy.wmorrison@gmail.com>
2017-07-21 02:33:16 -0700

    inputs for tanks are done\!

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle) {
+    UE_LOG(LogTemp, Warning, TEXT("Throttling %f"), Throttle);
+}
+
---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), hidecategories=(Collision) )
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category=Input)
+    void SetThrottle(float Throttle);
+};
